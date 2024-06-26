31de5b69bb94fd5006fa491dd61df5c646a8eef5
Juan C. Caicedo <7527135+jccaicedo@users.noreply.github.com>
2024-06-26 17:34:52 -0500

    Fixing paths

--- deepprofiler/fix_paths.py
+++ deepprofiler/fix_paths.py
@@ -9,9 +9,10 @@ root = sys.argv[1]
 
 for dir in os.listdir(root):
     print(dir)
-    offending = os.listdir(os.path.join(root, dir, "workspace_dl/embeddings"))
+    offending = [x for x in os.listdir(os.path.join(root, dir, "workspace_dl", "embeddings")) if x.find("Patining") != -1]
     for o in offending:
         path_from = os.path.join(root, dir, "workspace_dl/embeddings", o)
         path_to = path_from.replace("Cell_Patining_CNN", "Cell_Painting_CNN")
-        print(path_to)
-        os.system(f"mv {path_from} {path_to}")
+        instruction = f"mv {path_from} {path_to}"
+        print(instruction)
+        os.system(instruction)
